e5e20e660a320801c08b33a1de0122a521b99cd9
burle123 <burleshantanu@gmail.com>
2026-03-06 14:05:06 +0530

    Day 1

---
+++ Constant.py
@@ -0,0 +1,9 @@
+from all_constant import *
+
+present_count = 25
+absent_count = BATCH_SIZE - present_count
+print("Today's Details:")
+print("Batch Name:",BATCH_NAME)
+print("Total Students:", BATCH_SIZE)
+print("Total present students:",present_count)
+print("Total absent student:",absent_count)
---
+++ constant2.py
@@ -0,0 +1,8 @@
+from all_constant import BATCH_NAME, BATCH_SIZE
+present_count = 25
+absent_count = BATCH_SIZE - present_count
+print("Today's Details:")
+print("Batch Name:",BATCH_NAME)
+print("Total Students:", BATCH_SIZE)
+print("Total present students:",present_count)
+print("Total absent student:",absent_count)
