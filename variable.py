90f12cf7d49c4867d4947d583c73717318accd5d
burle123 <burleshantanu@gmail.com>
2026-03-06 14:54:50 +0530

    Day 1

--- variable.py
+++ variable.py
@@ -2,15 +2,21 @@ name= "Shantanu Burle"
 age = 22
 city = "Pune"
 height = 5.8
+isPass = True
+SGPA = 9.25
 weight = 74
 print("My name is", name)
 print("I am", age, "years old")
 print("I live in", city)
 print("My height is", height, "feet")
 print("My weight is", weight, "kg")
+print("Did I pass?", isPass)
+print("My SGPA is", SGPA)
 
 print(type(name))
 print(type(age))
 print(type(city))
 print(type(height))
 print(type(weight))
+print(type(isPass))
+print(type(SGPA))
