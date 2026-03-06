566d8b74a529b29251d1cf5481882c3edc5aee57
burle123 <burleshantanu@gmail.com>
2026-03-06 14:12:54 +0530

    Day 1

---
+++ areaofcircle.py
@@ -0,0 +1,10 @@
+print("Area of Circle")
+
+from all_constant import PI
+radius = 5
+circumference = 2*PI*radius
+area = PI * radius * radius
+print("Area of Circle with radius",radius,"is",area)
+print("Circumference of Circle",radius, "is",circumference)
+
+
