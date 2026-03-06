32241b35af87514a8da43e7739a4e8ecb9d98e7c
burle123 <burleshantanu@gmail.com>
2026-03-06 14:48:21 +0530

    Day 1

---
+++ variable.py
@@ -0,0 +1,16 @@
+name= "Shantanu Burle"
+age = 22
+city = "Pune"
+height = 5.8
+weight = 74
+print("My name is", name)
+print("I am", age, "years old")
+print("I live in", city)
+print("My height is", height, "feet")
+print("My weight is", weight, "kg")
+
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(height))
+print(type(weight))
