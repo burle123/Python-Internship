9391a5828708dfffe178d0a80e8cf0defc3ef17d
burle123 <burleshantanu@gmail.com>
2026-03-28 12:14:22 +0530

    Tuple

--- Tuple/class3.py
+++ Tuple/class3.py
@@ -1,1 +1,10 @@
-student=()
+#Tuple
+
+student=("Alice", 20, "Computer Science")
+print(student)
+print(student[0])  # Accessing the first element
+print(student[1])  # Accessing the second element
+
+#Tuples are immutable, so you cannot change their elements
+# student[1] = 21  # This will raise a TypeError
+
